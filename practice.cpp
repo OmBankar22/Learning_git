57f5c4e8aa4f10a0ba4aca43baa635f1cc7c3286
OmBankar22 <109789586+OmBankar22@users.noreply.github.com>
2023-06-13 18:35:52 +0530

    Create practice.cpp
    This is a simple example of finding square of any given number using cpp or c++

---
+++ practice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int no,ans;
+cout<<"Enter a number for square: ";
+  cin>>no;
+  ans = no*no;
+ cout<<"your square is: "<<ans;
+  
+return 0;
+}
